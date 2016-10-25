e1c91302ecab0c3c6472871c0a594e87b5549e9e
Utopia03 <agathe.blaise@isen.yncrea.fr>
2016-10-25 15:00:33 +0200

    Layout of main menu improved

--- app/src/main/java/com/example/agathe/tsgtest/MainActivity.java
+++ app/src/main/java/com/example/agathe/tsgtest/MainActivity.java
@@ -15,7 +15,7 @@ import com.olab.smplibrary.SMPLibrary;
 public class MainActivity extends AppCompatActivity {
 
 
-    Button login_button, logout_button, contacts_button, business_button, private_button;
+    Button login_button, logout_button;
     TextView login_field, contents;
     Context context;
     @Override
@@ -26,11 +26,7 @@ public class MainActivity extends AppCompatActivity {
         context = this;
         login_button = (Button) findViewById( R.id.login_but) ;
         logout_button = (Button) findViewById( R.id.logout_but);
-        contacts_button = (Button) findViewById( R.id.getcontcts);
-        business_button = (Button) findViewById( R.id.getbuisness);
-        private_button = (Button) findViewById( R.id.getprivate);
         login_field = (TextView) findViewById( R.id.login);
-        contents = (TextView) findViewById( R.id.contents);
         //  Library initialisation is required to be done once before any library function is called.
         //  You use your clientId and secret obtained from SMP website at developer tab.
         SMPLibrary.Initialise(this, "0000", "0000");
@@ -66,60 +62,6 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         login_field.setText("Not Logged In");
-                    }
-                });
-            }
-        });
-
-        //  setup business contacts button
-        business_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //  create new request for business contacts.
-                SMPLibrary.GetBusinessContacts(context, 10, new DataResponseCallback(){
-                    @Override
-                    //  OnResponse callback is called when request is finished processing
-                    //  returns response code and JSON in form of string.
-                    public void OnResponse( int response_code, String data_response ){
-                        Log.i("MainActivity:Response", "GetBusinessContacts response code " + response_code );
-                        Log.i("MainActivity:Response", "GetBusinessContacts - " + data_response);
-                        ShowMessage("Business Contacts\n" + data_response);
-                    }
-                });
-            }
-        });
-
-        //  //  setup private contacts button
-        private_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //  create new request for private contacts.
-                SMPLibrary.GetPrivateContacts(context, 10, new DataResponseCallback(){
-                    @Override
-                    //  OnResponse callback is called when request is finished processing
-                    //  returns response code and JSON in form of string.
-                    public void OnResponse( int response_code, String data_response ){
-                        Log.i("MainActivity:Response", "GetPrivateContacts response code" + response_code );
-                        Log.i("MainActivity:Response", "GetPrivateContacts - " + data_response);
-                        ShowMessage("Private Contacts\n" + data_response);
-                    }
-                });
-            }
-        });
-
-        //  setup frequent contacts button
-        contacts_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //  create new request for frequent contacts.
-                SMPLibrary.GetFrequentContacts(context, 10, new DataResponseCallback(){
-                    @Override
-                    //  OnResponse callback is called when request is finished processing
-                    //  returns response code and JSON in form of string.
-                    public void OnResponse( int response_code, String data_response ){
-                        Log.i("MainActivity:Response", "GetFrequentContacts response code " + response_code );
-                        Log.i("MainActivity:Response", "GetFrequentContacts - " + data_response);
-                        ShowMessage("Frequent Contacts\n" + data_response);
                     }
                 });
             }
