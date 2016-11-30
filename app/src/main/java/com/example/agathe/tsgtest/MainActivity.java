a1825b6404a44fef335e1de48b6787941436ac39
gdc0411 <danchao.gu@isen-lille.fr>
2016-11-30 15:55:38 +0100

    add push notification
    succeed to use AWS SNS(simple notification service) and receive the
    cloud message

--- app/src/main/java/com/example/agathe/tsgtest/MainActivity.java
+++ app/src/main/java/com/example/agathe/tsgtest/MainActivity.java
@@ -1,22 +1,40 @@
 package com.example.agathe.tsgtest;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.AsyncTask;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.mobile.AWSMobileClient;
+import com.amazonaws.mobile.push.PushManager;
 import com.olab.smplibrary.DataResponseCallback;
 import com.olab.smplibrary.LoginResponseCallback;
 import com.olab.smplibrary.SMPLibrary;
 
 public class MainActivity extends AppCompatActivity {
 
+    //*************AWS push notification part start************
+    /** Class name for log messages. */
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    // if you want to take action when a google services result is received.
+    private static final int REQUEST_GOOGLE_PLAY_SERVICES = 1363;
+    private PushManager pushManager;
+    private CheckBox enablePushCheckBox;
 
+    //*************AWS push notification part end**************
     Button login_button, logout_button, goto_PublicEvents;
     TextView login_field, contents;
     Context context;
@@ -28,6 +46,27 @@ public class MainActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
+        //*************AWS push notification part start************
+        enablePushCheckBox = (CheckBox) findViewById(R.id.enable_push_checkbox);
+        // Obtain a reference to the mobile client. It is created in the Application class,
+        // but in case a custom Application class is not used, we initialize it here if necessary.
+        AWSMobileClient.initializeMobileClientIfNecessary(this);
+
+        // Obtain a reference to the mobile client. It is created in the Application class.
+        final AWSMobileClient awsMobileClient = AWSMobileClient.defaultMobileClient();
+
+        pushManager = AWSMobileClient.defaultMobileClient().getPushManager();
+
+        enablePushCheckBox.setChecked(pushManager.isPushEnabled());
+        enablePushCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleNotification(enablePushCheckBox.isChecked());
+            }
+        });
+        //*************AWS push notification part end**************
+
+        // go to public events activity
         goto_PublicEvents = (Button)findViewById(R.id.pevents_button);
         goto_PublicEvents.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -90,6 +129,82 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+        //*************AWS push notification part start************
+        final AWSMobileClient awsMobileClient = AWSMobileClient.defaultMobileClient();
+
+        // register notification receiver
+        LocalBroadcastManager.getInstance(this).registerReceiver(notificationReceiver,
+                new IntentFilter(PushListenerService.ACTION_SNS_NOTIFICATION));
+
+        //*************AWS push notification part end**************
+    }
+    //*************AWS push notification part start************
+    private final BroadcastReceiver notificationReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(LOG_TAG, "Received notification from local broadcast. Display it in a dialog.");
+
+            Bundle data = intent.getBundleExtra(PushListenerService.INTENT_SNS_NOTIFICATION_DATA);
+            String message = PushListenerService.getMessage(data);
+
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle(R.string.push_demo_title)
+                    .setMessage(message)
+                    .setPositiveButton(android.R.string.ok, null)
+                    .show();
+        }
+    };
+
+    private void toggleNotification(final boolean enabled) {
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(final Void... params) {
+                // register device first to ensure we have a push endpoint.
+                pushManager.registerDevice();
+
+                // if registration succeeded.
+                if (pushManager.isRegistered()) {
+                    try {
+                        pushManager.setPushEnabled(enabled);
+                        // Automatically subscribe to the default SNS topic
+                        if (enabled) {
+                            pushManager.subscribeToTopic(pushManager.getDefaultTopic());
+                        }
+                        return null;
+                    } catch (final AmazonClientException ace) {
+                        Log.e(LOG_TAG, "Failed to change push notification status", ace);
+                        return ace.getMessage();
+                    }
+                }
+                return "Failed to register for push notifications.";
+            }
+
+            @Override
+            protected void onPostExecute(final String errorMessage) {
+                enablePushCheckBox.setChecked(pushManager.isPushEnabled());
+                System.out.println((errorMessage == null) ? "Register succeed......" : "Fail to register......" + errorMessage);
+                if (errorMessage != null) {
+                    showErrorMessage(R.string.error_message_update_notification, errorMessage);
+                }
+            }
+        }.execute();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // unregister notification receiver
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(notificationReceiver);
+    }
+    //*************AWS push notification part end**************
+
+
     //  function displaying message in TextView box.
     void ShowMessage(String message){
         final String to_show = message;
@@ -104,12 +219,14 @@ public class MainActivity extends AppCompatActivity {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                login_field.setText("Logged as: " + SMPLibrary.LoggedUserName() );
+                login_field.setText("Logged as: " + SMPLibrary.LoggedUserName());
             }
         });
     }
 
-
-
-
+    private AlertDialog showErrorMessage(final int resId, final Object... args) {
+        return new AlertDialog.Builder(this).setMessage(getString(resId, (Object[]) args))
+                .setPositiveButton(android.R.string.ok, null)
+                .show();
+    }
 }
--- app/src/main/java/com/example/agathe/tsgtest/PublicEventsActivity.java
+++ app/src/main/java/com/example/agathe/tsgtest/PublicEventsActivity.java
@@ -1,15 +1,25 @@
 package com.example.agathe.tsgtest;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.olab.smplibrary.DataResponseCallback;
+import com.olab.smplibrary.LoginResponseCallback;
+import com.olab.smplibrary.SMPLibrary;
 
 public class PublicEventsActivity extends AppCompatActivity {
 
+    Context context;
+    TextView view_test, view_test_1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +38,23 @@ public class PublicEventsActivity extends AppCompatActivity {
             getFragmentManager().beginTransaction().add(R.id.container, new PublicEventsFragment()).commit();
         }
 
+        context = this;
+        view_test = (TextView) findViewById( R.id.pe_view_test);
+        view_test_1 = (TextView) findViewById( R.id.pe_view_test_1);
+        String test_output = "Logged as: " + SMPLibrary.LoggedUserName();
+        test_output += "Is logged in: " + SMPLibrary.IsLoggedIn();
+        SMPLibrary.GetFrequentContacts(context, 10, new DataResponseCallback(){
+            @Override
+            //  OnResponse callback is called when request is finished processing
+            //  returns response code and JSON in form of string.
+            public void OnResponse( int response_code, String data_response ){
+                Log.i("PublicEvents:Response", "GetFrequentContacts response code " + response_code );
+                Log.i("PublicEvents:Respwonse", "GetFrequentContacts - " + data_response);
+                ShowMessage("Frequent Contacts\n" + data_response);
+            }
+        });
+        view_test.setText(test_output);
+
     }
 
     @Override
@@ -40,6 +67,8 @@ public class PublicEventsActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_disconnect:
+                SMPLibrary.Logout();
+                refresh();
                 return true;
 
             case R.id.action_about:
@@ -59,4 +88,22 @@ public class PublicEventsActivity extends AppCompatActivity {
 
         }
     }
+
+    //  function displaying message in TextView box.
+    void ShowMessage(String message){
+        final String to_show = message;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                view_test_1.setText( to_show );
+            }
+        });
+    }
+
+    private void refresh() {
+        finish();
+        Intent intent = new Intent(PublicEventsActivity.this, PublicEventsActivity.class);
+        startActivity(intent);
+    }
+
 }
